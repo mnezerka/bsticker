2ae89ca4236ac88271ca80f52d22c6e4b6cd839e
michal.nezerka@gmail.com <michal.nezerka@gmail.com@49e45a95-6085-8100-6333-f7221cd53f7d>
2013-06-04 12:45:34 +0000

    implemented: new about dialog, drawing of beat notes, loading and saving presets, tap tempo functionality

--- trunk/src/com/bluesoft/android/DialogSave.java
+++ trunk/src/com/bluesoft/android/DialogSave.java
@@ -1,5 +1,6 @@
 package com.bluesoft.android;
 
+import java.util.ArrayList;
 import android.app.Dialog;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -9,8 +10,12 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView; 
 
 public class DialogSave extends DialogFragment
 {
@@ -36,6 +41,25 @@ public class DialogSave extends DialogFragment
 
 		final EditText ctrlPresetName = (EditText)v.findViewById(R.id.preset);
 		ctrlPresetName.setText(mPresetName);
+
+		ListView filesCtrl = (ListView)v.findViewById(R.id.presets);
+
+		filesCtrl.setClickable(true);
+		filesCtrl.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			{
+				mPresetName = ((TextView)view).getText().toString();
+				BSTicker activity = (BSTicker) getActivity();
+				activity.onFinishDialogSave(DialogSave.this);
+				DialogSave.this.getDialog().dismiss();
+			}
+		});
+
+		ArrayList<String> presetFiles = ((BSTicker)getActivity()).getPresetNames();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, presetFiles);
+		filesCtrl.setAdapter(adapter);
 
 		// watch for button clicks.
 		final Button btnOk = (Button)v.findViewById(R.id.dialogButtonOK);
